1a0b10d3f7c8edc6d69ec779af6e7bb41ad5172e
buraxta <burakgulluler@gmail.com>
2023-09-21 12:55:22 +0300

    create the news section of the widgets component

--- app/page.js
+++ app/page.js
@@ -2,21 +2,36 @@ import Feed from "@/components/Feed";
 import Sidebar from "@/components/Sidebar";
 import Widgets from "@/components/Widgets";
 
-export default function Home() {
+export default async function Home() {
+  async function getData() {
+    const res = await fetch(
+      "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const data = await res.json();
+    return data;
+  }
+
+  const newsResults = await getData();
+  console.log("results", newsResults.status);
+
   return (
     <main
       className="grid grid-cols-4 min-h-screen max-w-6xl mx-auto
-    "
+      "
     >
       <Sidebar className=" w-full" />
       <div
         className="col-span-4 lg:col-span-3 border-[2px] border-gray-200
-        dark:border-gray-700
-      ml-[100px] lg:ml-[300px] max-w-xl lg:min-w-[550px]"
+          dark:border-gray-700
+        ml-[100px] lg:ml-[300px] max-w-xl lg:min-w-[550px]"
       >
         <Feed />
       </div>
-      <Widgets className="col-span-1 " />
+      <Widgets newsResults={newsResults.articles} />
     </main>
   );
 }
--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -1,9 +1,12 @@
+"use client";
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import News from "./News";
 
-const Widgets = () => {
+const Widgets = ({ newsResults }) => {
+  const [articleNumber, setArticleNumber] = React.useState(3);
   return (
-    <div className="xl:w-[350px] hidden lg:inline ml-8 space-y-5 bg-red-300">
+    <div className="xl:w-[350px] hidden lg:inline ml-8 space-y-5 ">
       <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
         <div className="flex items-center p-3 rounded-full relative">
           <AiOutlineSearch
@@ -18,6 +21,21 @@ const Widgets = () => {
             focus:bg-white bg-gray-100"
           />
         </div>
+      </div>
+      <div
+        className="text-gray-700 space-y-3 bg-gray-100 rounded-xl
+      pt-2 w-[90%] xl:w-[75%]]"
+      >
+        <h4 className="font-bold text-xl px-4">What's happening?</h4>
+        {newsResults.slice(0, articleNumber).map((article) => (
+          <News key={article.title} article={article} />
+        ))}
+        <button
+          onClick={() => setArticleNumber(articleNumber + 3)}
+          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+        >
+          Show more
+        </button>
       </div>
     </div>
   );
